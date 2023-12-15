d1b13fe3a9523fcd12a34c09a6cd6a8e6f45cf29
Blkalkin <88650606+Blkalkin@users.noreply.github.com>
2023-12-15 08:56:14 -0800

    axios not imported

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './TopBar.css'; // Make sure to create this CSS file
 import tdlogo from './tdlogo.svg';
 import { useNavigate } from 'react-router-dom';
