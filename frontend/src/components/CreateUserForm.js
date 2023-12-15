ff5aaa0204631d73efbce38be505499754ce3d01
Blkalkin <88650606+Blkalkin@users.noreply.github.com>
2023-12-15 09:32:10 -0800

    dashboard update

--- frontend/src/components/CreateUserForm.js
+++ frontend/src/components/CreateUserForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './CreateUserForm.css'; // Import the CSS file here
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CreateUserForm = () => {
   const [username, setUsername] = useState('');
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,59 +4,68 @@ import TopBar from './TopBar';
 import './Dashboard.css';
 
 const Dashboard = () => {
-  const [portfolio, setPortfolio] = useState(null);
+  const [portfolio, setPortfolio] = useState({ stocks: [] });
   const [marketStocks, setMarketStocks] = useState([]);
+  const [selectedBuyQuantity, setSelectedBuyQuantity] = useState({});
+  const [selectedSellQuantity, setSelectedSellQuantity] = useState({});
 
   useEffect(() => {
-    // Set withCredentials to true for axios to send cookies with requests
     axios.defaults.withCredentials = true;
 
-    // Fetch portfolio data
     axios.get('/portfolios/1')
-      .then(response => {
-        setPortfolio(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching portfolio data', error);
-      });
+      .then(response => setPortfolio(response.data))
+      .catch(error => console.error('Error fetching portfolio data', error));
 
-    // Fetch market stocks data
     axios.get('/market_stocks')
-      .then(response => {
-        setMarketStocks(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching market stocks data', error);
-      });
+      .then(response => setMarketStocks(response.data))
+      .catch(error => console.error('Error fetching market stocks data', error));
   }, []);
 
-  const handleBuy = (tickerSymbol, quantity) => {
-    axios.post(`/portfolios/1/stocks/buy`, {
-      ticker_symbol: tickerSymbol,
-      quantity: quantity
-    }).then(response => {
-      // Update the portfolio data after buying
-      setPortfolio(prevPortfolio => ({
-        ...prevPortfolio,
-        stocks: [...prevPortfolio.stocks, response.data]
-      }));
-    }).catch(error => {
-      console.error('Error buying stock', error);
-    });
+  const handleBuy = (tickerSymbol) => {
+    const quantity = selectedBuyQuantity[tickerSymbol];
+    if (quantity > 0) {
+      axios.post(`/portfolios/1/stocks/buy`, {
+        ticker_symbol: tickerSymbol,
+        quantity: quantity
+      })
+      .then(response => {
+        setPortfolio(prevPortfolio => ({
+          ...prevPortfolio,
+          stocks: [...prevPortfolio.stocks, response.data.stock],
+          current_cash: response.data.current_cash
+        }));
+        // Reset the selected quantity after buying
+        setSelectedBuyQuantity(prevQuantities => ({
+          ...prevQuantities,
+          [tickerSymbol]: ''
+        }));
+      })
+      .catch(error => console.error('Error buying stock', error));
+    }
   };
 
-  const handleSell = (stockId, quantity) => {
-    axios.delete(`/portfolios/1/stocks/${stockId}/sell`, {
-      data: { quantity: quantity }
-    }).then(response => {
-      // Update the portfolio data after selling
-      setPortfolio(prevPortfolio => ({
-        ...prevPortfolio,
-        stocks: prevPortfolio.stocks.filter(stock => stock.id !== stockId || stock.quantity > quantity)
-      }));
-    }).catch(error => {
-      console.error('Error selling stock', error);
-    });
+  const handleSell = (stockId) => {
+    const quantity = selectedSellQuantity[stockId];
+    if (quantity > 0) {
+      axios.delete(`/portfolios/1/stocks/${stockId}/sell`, {
+        data: { quantity: quantity }
+      })
+      .then(response => {
+        setPortfolio(prevPortfolio => ({
+          ...prevPortfolio,
+          stocks: prevPortfolio.stocks.map(stock =>
+            stock.id === stockId ? { ...stock, quantity: stock.quantity - quantity } : stock
+          ).filter(stock => stock.quantity > 0),
+          current_cash: response.data.current_cash
+        }));
+        // Reset the selected quantity after selling
+        setSelectedSellQuantity(prevQuantities => ({
+          ...prevQuantities,
+          [stockId]: ''
+        }));
+      })
+      .catch(error => console.error('Error selling stock', error));
+    }
   };
 
   return (
@@ -65,8 +74,8 @@ const Dashboard = () => {
       <div className="dashboard-container">
         <section className="account-summary">
           <h2>Account Summary</h2>
-          <div>Account Value: ${portfolio?.total_value}</div>
-          <div>Buying Power: ${portfolio?.current_cash}</div>
+          <div>Account Value: ${portfolio.total_value}</div>
+          <div>Buying Power: ${portfolio.current_cash}</div>
         </section>
 
         <section className="market-stocks">
@@ -75,7 +84,16 @@ const Dashboard = () => {
             {marketStocks.map(stock => (
               <li key={stock.id}>
                 {stock.name} (${stock.current_price})
-                <button onClick={() => handleBuy(stock.id)}>Buy</button>
+                <input
+                  type="number"
+                  value={selectedBuyQuantity[stock.ticker_symbol] || ''}
+                  onChange={(e) => setSelectedBuyQuantity({
+                    ...selectedBuyQuantity,
+                    [stock.ticker_symbol]: e.target.value
+                  })}
+                  placeholder="Quantity"
+                />
+                <button onClick={() => handleBuy(stock.ticker_symbol)}>Buy</button>
               </li>
             ))}
           </ul>
@@ -85,14 +103,27 @@ const Dashboard = () => {
           <h2>Individual Positions</h2>
           <table>
             <thead>
-              {/* ... table headers ... */}
+              <tr>
+                <th>Position</th>
+                <th>Qty</th>
+                <th>Action</th>
+              </tr>
             </thead>
             <tbody>
-              {portfolio?.stocks.map(stock => (
+              {portfolio.stocks.map(stock => (
                 <tr key={stock.id}>
                   <td>{stock.name}</td>
-                  {/* ... other stock details ... */}
+                  <td>{stock.quantity}</td>
                   <td>
+                    <input
+                      type="number"
+                      value={selectedSellQuantity[stock.id] || ''}
+                      onChange={(e) => setSelectedSellQuantity({
+                        ...selectedSellQuantity,
+                        [stock.id]: e.target.value
+                      })}
+                      placeholder="Quantity"
+                    />
                     <button onClick={() => handleSell(stock.id)}>Sell</button>
                   </td>
                 </tr>
@@ -106,4 +137,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
